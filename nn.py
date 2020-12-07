c26c5736f7646fd4c310b5692ded3680a918ca91
shaun-cmu <60110416+Shaun-CMU@users.noreply.github.com>
2020-12-07 18:30:49 -0500

    run nn.py to see it learn over additional generations

--- nn.py
+++ nn.py
@@ -30,18 +30,26 @@ def Plot_stuff(x1,y1,x2,y2):
 lr_param = Parameter([0.0, .2])
 maxGradNormParam = Parameter([0, 1])
 params = [lr_param, maxGradNormParam]
-number_o_samples = 300 
+number_o_samples = 300
 temp = Sampler('grid',len(params),number_o_samples)
 X_test = np.array(temp.getSamples(params,numSamples=number_o_samples))
 
 
-X_train = np.loadtxt("Xvals.txt")
-Y_train = np.loadtxt("Yvals.txt")
-clf = MLPRegressor(hidden_layer_sizes=(100,100),activation='relu', max_iter=3*len(Y_train), alpha=0.001,
-                     solver='lbfgs', learning_rate='adaptive', verbose=1,  random_state=9)
 
-clf.fit(X_train.astype('float'), Y_train.astype('float64').ravel())
-Y_test = clf.predict(X_test.astype('float'))
+X_train = np.loadtxt("Xvals.txt")[10*0:10*(0+1),:]
+Y_train = np.loadtxt("Yvals.txt")[10*0:10*(0+1)]
+for i in range(1,9):
+    newx = np.loadtxt("Xvals.txt")[10*i:10*(i+1),:]
+    newy = np.loadtxt("Yvals.txt")[10*i:10*(i+1)]
+    X_train = np.append(X_train,newx,axis=0)
+    Y_train = np.append(Y_train,newy,axis=0)
+    clf = MLPRegressor(hidden_layer_sizes=(100,100),activation='relu', max_iter=3*len(Y_train), alpha=2,
+                        solver='lbfgs', learning_rate='adaptive', verbose=1,  random_state=9)
+
+    clf.fit(np.array(X_train).astype('float'), np.array(Y_train).astype('float64').ravel())
+    Y_test = clf.predict(np.array(X_test).astype('float'))
+    Y_test = np.array(Y_test)
+    Plot_stuff(X_train,Y_train,X_test,Y_test)
 
 # clf = MLPClassifier(hidden_layer_sizes=(100,100),activation='relu', max_iter=1, alpha=20,
 #                      solver='lbfgs', learning_rate='adaptive', verbose=1,  random_state=9)
@@ -51,7 +59,6 @@ Y_test = clf.predict(X_test.astype('float'))
 
 
 
-Y_test = np.array(Y_test)
 
 
 
@@ -61,7 +68,7 @@ Y_test = np.array(Y_test)
 # r = np.sqrt(x**2 + y**2)
 # z = np.sin(r)
 
-Plot_stuff(X_train,Y_train,X_test,Y_test)
+
 
 
 print("done")
